b985bd144db2513e00a43a679ea6eb8ace9f24e7
TheWolfKnight <philipalexknudsen@gmail.com>
2022-02-07 09:40:29 +0100

    commit #2 (Made the fileReader and started on the cleaner function)

--- src/util.py
+++ src/util.py
@@ -3,6 +3,42 @@ if __name__ == "__main__":
 	exit(1)
 
 
-def cleaner(li: list[list[chr]]) -> list[list[chr]]:
-	pass
+def readFile(fName: str, path_prefix: str="'/") -> list[list[chr]]:
+	r: list[list[chr]] = []
+	data: str = None
+	with open(f"tmp.txt") as fp:
+		data: str = fp.read()
+	assert data, "No data in file"
+	tmp: list[chr] = []
+	for c in data:
+		if c == "\n":
+			r.append(tmp.copy())
+			tmp = []
+			continue
+
+		tmp.append(c)
+	if tmp:
+		r.append(tmp.copy())
+	return r
+
+
+def cleaner(li2d: list[list[chr]]) -> list[list[chr]]:
+	r: list[list[chr]] = []
+	for li in li2d:
+		tmp: list[chr] = []
+		for c in li:
+			if validChr(c):
+				tmp.append(c)
+			else:
+				raise ValueError
+		if r:
+			assert len(tmp)+2 == len(r[0]), "The dimensions of all nested lists, must be equal"
+		tmp.insert(0, 'X')
+		tmp.append('X')
+		r.append(tmp.copy())
+		tmp = []
+	i: int = r[0]
+	r.insert(0, ['X']*len(i))
+	r.append(['X']*len(i))
+	return r
 
