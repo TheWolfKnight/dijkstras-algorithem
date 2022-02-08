fafbb146e221f05b2a07df988243eb1316d5d399
TheWolfKnight <philipalexknudsen@gmail.com>
2022-02-08 12:55:05 +0100

    commit #3 (comment on the different pieces of code in the prog as of yet)

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
 	pass
 
 
+# safeguards against the program being run in a "wrong" way
+# in all other files this will just run the "exit(1)" command
 if __name__ == '__main__':
 	main()
 	exit(0)
--- src/typ.py
+++ src/typ.py
@@ -6,9 +6,21 @@ if __name__ == '__main__':
 from enum import Enum, auto
 
 
-class Tiles(Enum):
+# This class will holde all the different types of tiles for the prog.
+# This is used when there is a need to enumerate through the tiles.
+class Tile(Enum):
 	Start	= auto()
 	Stop	= auto()
 	Open 	= auto()
 	Closed 	= auto()
+
+
+# This table will make looking up the different tiles easy for the prog.
+# To extend this simply add a new char with the corresponding tile
+CHAR_TO_TILE_TABLE: dict[chr, Tile] = {
+	"G": Tile.Start,
+	"S": Tile.Stop,
+	"O": Tile.Open,
+	"X": Tile.Closed,
+}
 
